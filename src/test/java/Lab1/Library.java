cd512bc1a1f8e846cb28713c2b3e2329b28d8ae0
Junxian_Luke_Lu <32172402+Luke-lujunxian@users.noreply.github.com>
2020-02-25 11:33:54 +0800

    some changes to fit requirement

---
+++ src/test/java/Lab1/Library.java
@@ -0,0 +1,12 @@
+package Lab1;
+
+public class Library {
+    public static void main(String arg[]){
+        Library lib = new Library();
+        if(lib.someLibraryMethod())
+            System.out.println("When There is a fire, commit and push!");
+    }
+    public boolean someLibraryMethod(){
+        return true;
+    }
+}
