7add867d12f6a308f3d0a397760d35b4453da826
Junxian_Luke_Lu <32172402+Luke-lujunxian@users.noreply.github.com>
2020-02-25 11:16:59 +0800

    sth

---
+++ src/main/java/Lab1/Library.java
@@ -0,0 +1,10 @@
+package Lab1;
+
+public class Library {
+    public static void main(String arg[]){
+        System.out.println("When There is a fire, commit and push");
+    }
+    public boolean someLibraryMethod(){
+         return true;
+    }
+}
